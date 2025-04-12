2768cc7fad6a7faf56ec1bd9606e79179ac8b0d2
chhong <pnci1029@gmail.com>
2025-04-12 14:52:06 +0900

    시크릿 수정

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Configure axios defaults
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:3000';
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 // Add request interceptor
